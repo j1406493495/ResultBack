e28e32cf3b9087425facc84b891b5310e8be0b2d
Woong <jinwanghui@abcpen.com>
2018-02-24 16:07:19 +0800

    [Bug XXXX] rxbinding Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/com/woong/resultbackdemo/MainActivity.java
+++ app/src/main/java/cn/com/woong/resultbackdemo/MainActivity.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.jakewharton.rxbinding2.view.RxView;
+
 import cn.com.woong.resultbacklib.ResultBack;
 import cn.com.woong.resultbacklib.ResultInfo;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class MainActivity extends AppCompatActivity {
     Button btnFailed;
@@ -21,53 +26,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_main);
-        init();
+        initView();
+        initData();
     }
 
-    private void init() {
+    private void initView() {
         btnFailed = findViewById(R.id.btn_failed);
         btnSuccess = findViewById(R.id.btn_success);
+    }
 
+    private void initData() {
         mResultBack = new ResultBack(this);
 
-        btnFailed.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mResultBack.startForResult(ResultFailedActivity.class)
-                        .subscribe(new Consumer<ResultInfo>() {
-                            @Override
-                            public void accept(ResultInfo resultInfo) throws Exception {
-                                int resultCode = resultInfo.getResultCode();
-                                Intent data = resultInfo.getData();
+        RxView.clicks(btnFailed)
+                .flatMap(new Function<Object, Observable<ResultInfo>>() {
+                    @Override
+                    public Observable<ResultInfo> apply(Object o) throws Exception {
+                        return mResultBack.startForResult(ResultFailedActivity.class);
+                    }
+                })
+                .subscribe(new Consumer<ResultInfo>() {
+                    @Override
+                    public void accept(ResultInfo resultInfo) throws Exception {
+                        int resultCode = resultInfo.getResultCode();
+                        Intent data = resultInfo.getData();
 
-                                if (resultCode == RESULT_CANCELED) {
-                                    Toast.makeText(MainActivity.this, "Result failed back", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
+                        if (resultCode == RESULT_CANCELED) {
+                            Toast.makeText(MainActivity.this, "Result failed back", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        btnSuccess.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ResultSuccessActivity.class);
-                intent.putExtra("success", false);
+        RxView.clicks(btnSuccess)
+                .map(new Function<Object, Intent>() {
+                    @Override
+                    public Intent apply(Object o) throws Exception {
+                        Intent intent = new Intent(MainActivity.this, ResultSuccessActivity.class);
+                        intent.putExtra("success", false);
+                        return intent;
+                    }
+                })
+                .flatMap(new Function<Intent, Observable<ResultInfo>>() {
+                    @Override
+                    public Observable<ResultInfo> apply(Intent intent) throws Exception {
+                        return mResultBack.startForResult(intent);
+                    }
+                })
+                .subscribe(new Consumer<ResultInfo>() {
+                    @Override
+                    public void accept(ResultInfo resultInfo) throws Exception {
+                        int resultCode = resultInfo.getResultCode();
+                        Intent data = resultInfo.getData();
 
-                mResultBack.startForResult(intent)
-                        .subscribe(new Consumer<ResultInfo>() {
-                            @Override
-                            public void accept(ResultInfo resultInfo) throws Exception {
-                                int resultCode = resultInfo.getResultCode();
-                                Intent data = resultInfo.getData();
-
-                                if (resultCode == RESULT_OK && data != null) {
-                                    boolean result= data.getBooleanExtra("result", false);
-                                    Toast.makeText(MainActivity.this, "result == " + result, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-            }
-        });
+                        if (resultCode == RESULT_OK && data != null) {
+                            boolean result= data.getBooleanExtra("result", false);
+                            Toast.makeText(MainActivity.this, "result == " + result, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
