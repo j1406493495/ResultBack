e22cae50178c91960d7e97bd35e4182693a3b9c1
Woong <1406493495@qq.com>
2018-04-08 08:25:02 +0800

    [Bug XXXX] 代码格式 Cause: None Solution: None Fixed Version: None Branch: None

--- resultbacklib/src/main/java/cn/com/woong/resultbacklib/ResultBackFragment.java
+++ resultbacklib/src/main/java/cn/com/woong/resultbacklib/ResultBackFragment.java
@@ -9,32 +9,33 @@ import java.util.Map;
 
 /**
  * Created by woong on 2017/01/23.
+ *
  * @author woong
  */
 public class ResultBackFragment extends Fragment {
-private Map<Integer, ResultBack.Callback> mCallbacks = new HashMap<>();
+    private Map<Integer, ResultBack.Callback> mCallbacks = new HashMap<>();
 
-public ResultBackFragment() {
-}
-
-@Override
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setRetainInstance(true);
-}
-
-public void startForResult(Intent intent, ResultBack.Callback callback) {
-    mCallbacks.put(callback.hashCode(), callback);
-    startActivityForResult(intent, callback.hashCode());
-}
-
-@Override
-public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    //callback方式的处理
-    ResultBack.Callback callback = mCallbacks.remove(requestCode);
-    if (callback != null) {
-        callback.onActivityResult(new ResultInfo(resultCode, data));
+    public ResultBackFragment() {
     }
-}
+
+    public void startForResult(Intent intent, ResultBack.Callback callback) {
+        mCallbacks.put(callback.hashCode(), callback);
+        startActivityForResult(intent, callback.hashCode());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //callback方式的处理
+        ResultBack.Callback callback = mCallbacks.remove(requestCode);
+        if (callback != null) {
+            callback.onActivityResult(new ResultInfo(resultCode, data));
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 }
--- resultbacklib/src/main/java/cn/com/woong/resultbacklib/ResultInfo.java
+++ resultbacklib/src/main/java/cn/com/woong/resultbacklib/ResultInfo.java
@@ -4,30 +4,32 @@ import android.content.Intent;
 
 /**
  * Created by woong on 2018/01/23.
+ *
  * @author woong
  */
 public class ResultInfo {
-private int resultCode;
-private Intent data;
 
-public ResultInfo(int resultCode, Intent data) {
-    this.resultCode = resultCode;
-    this.data = data;
-}
+    private int resultCode;
+    private Intent data;
 
-public int getResultCode() {
-    return resultCode;
-}
+    public ResultInfo(int resultCode, Intent data) {
+        this.resultCode = resultCode;
+        this.data = data;
+    }
 
-public void setResultCode(int resultCode) {
-    this.resultCode = resultCode;
-}
+    public int getResultCode() {
+        return resultCode;
+    }
 
-public Intent getData() {
-    return data;
-}
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
 
-public void setData(Intent data) {
+    public Intent getData() {
+        return data;
+    }
+
+    public void setData(Intent data) {
         this.data = data;
     }
 }
